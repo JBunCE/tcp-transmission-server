954f499b4cee6e15abd8439fb2c8cabfa471aaee
Jorge Brandon Chandomi Esponda <jorge_b_chandomi@outlook.com>
2024-12-01 22:28:01 -0600

    update

--- src/broker.rs
+++ src/broker.rs
@@ -7,7 +7,7 @@ use crate::models::server_events::StreamEvent;
 
 
 pub async fn message_producer(payload: StreamEvent) ->  Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = env::var("RABBITMQ_ADDR").unwrap_or_else(|_| "amqp://guest:guest@localhost:5672".into());
+    let addr = env::var("RABBITMQ_ADDR").unwrap_or_else(|_| "amqp://guest:guest@50.19.40.173:5672".into());
     let conn = Connection::connect(&addr, ConnectionProperties::default()).await?;
 
     let channel = conn.create_channel().await?;
